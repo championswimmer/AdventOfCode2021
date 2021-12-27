6f332f403058b270e615e63bc0e574f34ab65801
Arnav Gupta <championswimmer@gmail.com>
2021-12-28 04:54:29 +0530

    setup rust scripts
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ day-01/solution.rs
@@ -0,0 +1,4 @@
+// cargo-deps: commons = { path = "../commons" }
+fn main() {
+    println!("{}", io::say_hello());
+}
--- run-solns.sh
+++ run-solns.sh
@@ -7,3 +7,6 @@ go run day-02/solution.go
 # run kotlin solutions
 kscript day-01/solution.kt
 kscript day-02/solution.kt
+
+# run rust solutions
+rust-script day-01/solution.rs
