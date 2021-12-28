815250fc547d82ee99254077d4ed94ec064db973
Arnav Gupta <championswimmer@gmail.com>
2021-12-28 05:31:53 +0530

    day 01 solution in rust
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- commons/io.rs
+++ commons/io.rs
@@ -1,3 +1,8 @@
-pub fn say_hello() -> String {
-    "Hello, world!".to_string()
+use std::fs::File;
+use std::io::{self, BufRead};
+
+
+pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>>{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
--- day-01/solution.rs
+++ day-01/solution.rs
@@ -1,4 +1,19 @@
 // cargo-deps: commons = { path = "../commons" }
 fn main() {
-    println!("{}", io::say_hello());
+    let lines = io::read_lines("day-01/input.txt");
+    let nums = lines
+        .unwrap()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    let mut threesums: Vec<i32> = vec![0; nums.len() - 2];
+    for i in 0..threesums.len() {
+        threesums[i] = nums[i] + nums[i + 1] + nums[i + 2];
+    }
+    let mut count = 0;
+    for i in 1..threesums.len() {
+        if threesums[i] > threesums[i - 1] {
+            count += 1;
+        }
+    }
+    println!("{}", count);
 }
