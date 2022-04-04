837a1ab5b69a821a9508efc2ce4bfa48bb14d3d6
Arnav Gupta <championswimmer@gmail.com>
2022-04-05 02:20:56 +0530

    solve day 03 in typescript
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ day-03/solution.ts
@@ -0,0 +1,36 @@
+import { readLines } from "../commons/io.ts";
+
+function part1(lines: Array<string>): number {
+  const rays = lines[0].length;
+  const points = lines.length;
+
+  const rayVals = new Array(rays).fill(0);
+
+  lines.forEach((line) => {
+    line.split("").forEach((char, index) => {
+      if (char === "1") rayVals[index]++;
+    });
+  });
+
+  const gammaRate = rayVals.map((val) => (val > points / 2) ? 1 : 0);
+  const epsilonRate = rayVals.map((val) => (val < points / 2) ? 1 : 0);
+
+  const gammaPower = parseInt(gammaRate.join(""), 2);
+  const epsilonPower = parseInt(epsilonRate.join(""), 2);
+
+  console.log(gammaPower);
+  console.log(epsilonPower);
+
+
+  return gammaPower * epsilonPower;
+}
+
+function main() {
+  const lines = readLines("day-03/input.txt");
+
+  const powerRating = part1(lines);
+  console.log(powerRating);
+
+}
+
+main();
